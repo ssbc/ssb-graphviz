981ed08561e580c3f6f34242a8388c7614bdcb69
mix irving <whimful@gmail.com>
2016-12-06 00:26:30 +1300

    physics tweak + edge color

--- index.js
+++ index.js
@@ -4,12 +4,28 @@ const fromJson = require('ngraph.fromjson')
 const Renderer = require('ngraph.pixel')
 const Graph = require('./graph')
 
+const config = {
+  physics: {
+    springLength : 80,
+    springCoeff : 0.00005,
+    gravity: -0.4,
+    theta : 0.4,
+    dragCoeff : 0.04
+  },
+  link: () => {
+    return {
+      fromColor: 0x000066,
+      toColor: 0x000066
+    }
+  }
+}
+
 module.exports = function (sbot, cb) {
   Graph(sbot, (err, data) => {
     if (err) return cb(err)
     const str = JSON.stringify(data)
     const graph = fromJson(str)
-    const display = Renderer(graph)
+    const display = Renderer(graph, config)
     cb(null, display)
   })
 }
